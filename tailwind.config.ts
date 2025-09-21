668ec1eef1760be91fae6576f9c6b7f3d94877dd
emhatech <etaqdiruljabbar@gmail.com>
2025-09-21 18:07:14 +0700

    tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,25 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        gradient: {
+          "0%, 100%": { "background-position": "0% 50%" },
+          "50%": { "background-position": "100% 50%" },
+        },
+      },
+      animation: {
+        marquee: "marquee 12s linear infinite",
+        gradient: "gradient 6s ease infinite",
+      },
+      backgroundSize: {
+        "300%": "300% 300%",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
